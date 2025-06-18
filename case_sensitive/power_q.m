55fa6b701c5db057c9ddf811104b6e1fe1092ec2
RichardAlves8 <richarddarwiin@gmail.com>
2025-06-17 21:14:41 -0300

    myfilter M.PQ

---
+++ case_sensitive/power_q.m
@@ -0,0 +1,19 @@
+Categorias = Table.FromRecords({
+        [Categoria="Filtro um", PalavrasChave={"F1"}],
+        [Categoria="Filtro dois", PalavrasChave={"F2", "F2 ALPH"}],
+        [Categoria="Filtro tres", PalavrasChave={"F3 F3 F33", "f3"}],
+        [Categoria="Filtro quatro", PalavrasChave={"F4"}],
+    }),
+
+// função
+    DetectarCategoria = (texto as text) as text =>
+        let
+            Checar = Table.SelectRows(Categorias, each List.AnyTrue(List.Transform([PalavrasChave], (p) => Text.Contains(texto, p)))),
+            Resultado = if Table.RowCount(Checar) > 0 then Checar{0}[Categoria] else "Outros"
+        in
+            Resultado,
+
+    #"Personalização Adicionada1" = Table.AddColumn(
+        #"Linhas Classificadas",
+        "Categoria",
+        each DetectarCategoria([Lançamento])
