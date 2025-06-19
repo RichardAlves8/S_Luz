d59736663218dc77fd49e30b1bc61169dc1add48
RichardAlves8 <richarddarwiin@gmail.com>
2025-06-18 22:10:59 -0300

    css?

--- case_sensitive/power_q.m
+++ case_sensitive/power_q.m
@@ -8,9 +8,13 @@ Categorias = Table.FromRecords({
 // função
     DetectarCategoria = (texto as text) as text =>
         let
-            Checar = Table.SelectRows(Categorias, each List.AnyTrue(List.Transform([PalavrasChave], (p) => Text.Contains(texto, p)))),
+            Checar = Table.SelectRows(
+                Categorias, each List.AnyTrue(L
+                ist.Transform([PalavrasChave], (p) => Text.Contains(texto, p))
+                )
+            ),
             Resultado = if Table.RowCount(Checar) > 0 then Checar{0}[Categoria] else "Outros"
-        in
+        in  
             Resultado,
 
     #"Personalização Adicionada1" = Table.AddColumn(
